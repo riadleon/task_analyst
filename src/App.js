3c4c3423f1a2dc5abb5a528be0e5c4b3967c317b
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2023-02-03 13:13:12 +0600

    some changes of directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import About from './Components/About/About';
-import Banner from './Components/Banner/Banner';
+
 import ErrorPage from './Components/ErrorPage/ErrorPage';
 import UserDetails from './Components/Users/UserDetails';
 import Users from './Components/Users/Users';
@@ -10,18 +9,11 @@ import Main from './layout/Main/Main';
 function App() {
   const router = createBrowserRouter([
     {
-      path:'/',
+      path:'/users',
       element: <Main></Main>,
       errorElement:<ErrorPage></ErrorPage>,
       children:[
-        {
-          path:'/',
-          element:<Banner></Banner>
-        },
-        {
-          path:'/about',
-          element:<About></About>
-        },
+       
         {
           path:'/users',
           loader:()=>fetch('https://jsonplaceholder.typicode.com/users'),
